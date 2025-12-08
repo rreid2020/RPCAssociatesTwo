4b91d892c69b43ae092e42404a301644f3a56088
Roger <roger.reid@rpcassociates.co>
2025-12-08 06:41:19 -0500

    Move contact section to right side of footer and add Roger Reid contact information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,21 +7,6 @@ const Footer: FC = () => {
     <footer className="footer">
       <div className="container">
         <div className="footer__content">
-          <div className="footer__contact">
-            <h3 className="footer__title">Contact</h3>
-            <div className="footer__contact-info">
-              <div className="footer__contact-item">
-                <span className="footer__contact-label">Email:</span>
-                <a href="mailto:info@rpcassociates.ca" className="footer__contact-link">
-                  info@rpcassociates.ca
-                </a>
-              </div>
-              <div className="footer__contact-item">
-                <span className="footer__contact-label">Service area:</span>
-                <span>Based in Canada · Serving clients remotely</span>
-              </div>
-            </div>
-          </div>
           <div className="footer__bottom">
             <div className="footer__copyright">
               © {currentYear} RPC Associates. All rights reserved.
@@ -38,6 +23,26 @@ const Footer: FC = () => {
                 </a>
               </li>
             </ul>
+          </div>
+          <div className="footer__contact">
+            <h3 className="footer__title">Contact</h3>
+            <div className="footer__contact-info">
+              <div className="footer__contact-item">
+                <span>Roger Reid, CPA, CMA, CGAP</span>
+              </div>
+              <div className="footer__contact-item">
+                <span className="footer__contact-label">Phone/Text:</span>
+                <a href="tel:6138840208" className="footer__contact-link">
+                  613-884-0208
+                </a>
+              </div>
+              <div className="footer__contact-item">
+                <span className="footer__contact-label">Email:</span>
+                <a href="mailto:roger.reid@rpcassociates.co" className="footer__contact-link">
+                  roger.reid@rpcassociates.co
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
